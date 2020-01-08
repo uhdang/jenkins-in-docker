26ab57a613fb5e6f2838b937bb0c4e43e915338b
jeong <uhdang@gmail.com>
2020-01-08 09:06:26 +0100

    detach added to docker command

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name jenkins-in-docker -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock uhdang/jenkins-in-docker:1
+docker run --name jenkins-in-docker -p 8080:8080 -d -v /var/run/docker.sock:/var/run/docker.sock uhdang/jenkins-in-docker:1
